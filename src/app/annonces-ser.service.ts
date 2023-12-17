0fd9ddae75193b3497ec8eb1e64d06d4be6acd4f
unknown <mohamed.meznihermi@esprit.tn>
2023-12-17 12:12:27 +0100

    for

--- src/app/annonces-ser.service.ts
+++ src/app/annonces-ser.service.ts
@@ -15,7 +15,9 @@ export class AnnoncesSerService {
       image:
         'https://banner2.cleanpng.com/20171201/7dd/audi-car-logo-png-brand-image-5a214d03605628.4914087715121318433946.jpg',
       category: 'vente',
-      description: '230 km',
+      couleur: 'blue',
+      annees: '150 000',
+      description: 'compteur 100 000km ',
     },
     {
       id: 2,
@@ -25,7 +27,9 @@ export class AnnoncesSerService {
       image:
         'https://png2.cleanpng.com/sh/23242d0c1ba4af4ad2ea981483d3b8c6/L0KzQYi4UsAyN5VofJGAYUK0RLTrgsA4QWU8SpC6M0m1R4K3WcE2OWM2S6MDMEOzQ4K4TwBvbz==/5a214cdb079472.1392710915121318030311.png',
       category: 'vente',
-      description: '230 km',
+      couleur: 'blue',
+      prix: '33000dt',
+      description: 'compteur 100 000km ',
     },
     {
       id: 3,
@@ -35,7 +39,9 @@ export class AnnoncesSerService {
       image:
         'https://banner2.cleanpng.com/20171220/ske/porsche-logo-png-5a3a1bdf5e2e58.06891294151375766338584256.jpg',
       category: 'rent',
-      description: '230 km',
+      couleur: 'noir',
+      prix: '200 000dt',
+      description: 'compteur 20 000km ',
     },
   ];
   
